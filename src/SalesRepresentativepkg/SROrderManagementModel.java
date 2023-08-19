9edc66a59c17857a0e6672d1b6a5a3d3431dcfcf
anika <anika@Anika>
2023-08-19 10:14:12 +0600

    added a comment

--- src/SalesRepresentativepkg/SROrderManagementModel.java
+++ src/SalesRepresentativepkg/SROrderManagementModel.java
@@ -71,4 +71,4 @@ public class SROrderManagementModel {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
-}
+}//kjabdkjnksljlkdcl
